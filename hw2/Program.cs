eea9c46135f67b1978bf87db6f476fb55aed232e
illustager <illustager@outlook.com>
2026-03-23 15:01:17 +0800

    hw2 完成

---
+++ hw2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace hw2 {
+    internal class Program {
+        static object GetStaticFieldValue(Type type, string fieldName) {
+            var field = type.GetField(fieldName, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+            return field?.GetValue(null);
+        }
+        static void Main() {
+            var types = new Type[] {
+                typeof(sbyte),
+                typeof(byte),
+                typeof(short),
+                typeof(ushort),
+                typeof(int),
+                typeof(uint),
+                typeof(long),
+                typeof(ulong),
+                typeof(float),
+                typeof(double),
+                typeof(decimal)
+            };
+
+            var sheetFormat = "|{0,8}|{1,5}|{2,31}|{3,30}|";
+            var sheetLine = new string[] {
+                new string('-', 8),
+                new string('-', 5),
+                new string('-', 31),
+                new string('-', 30)
+            };
+
+            Console.WriteLine(sheetFormat, "Type", "Size", "Min Value", "Max Value");
+            Console.WriteLine(sheetFormat, sheetLine);
+            foreach (var t in types) {
+                var name = t.Name;
+                var size = System.Runtime.InteropServices.Marshal.SizeOf(t);
+                var minValue = GetStaticFieldValue(t, "MinValue");
+                var maxValue = GetStaticFieldValue(t, "MaxValue");
+
+                Console.WriteLine(sheetFormat, name, size, minValue, maxValue);
+            }
+        }
+    }
+}
